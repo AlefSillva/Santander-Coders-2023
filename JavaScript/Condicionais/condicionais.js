7f908e0f2c50d8d8ea497256df2bf2ece9e4c324
AlefSillva <alefsilva0295@gmail.com>
2023-05-23 01:27:27 -0300

    aula de condicionais

---
+++ JavaScript/Condicionais/condicionais.js
@@ -0,0 +1,25 @@
+/* const idade = 15
+
+if (idade <= 18) {
+    console.log('Você é mucilon!')
+}else {
+    console.log('Você ja pode ser preso!')
+}
+*/
+
+console.clear()
+
+// let media = 4
+
+// if (media >= 7) {
+//     console.log('Aprovado(a)')
+// } else if (media >= 5) {
+//     console.log('recuperação')
+// }else {
+//     console.log('Reprovado(a)')
+// }
+
+var x = 10
+
+console.log(x + y)
+let y = 15
