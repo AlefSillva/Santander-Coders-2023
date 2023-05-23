55b17f52e82189427515b2126cb1fdb8d04750fc
AlefSillva <alefsilva0295@gmail.com>
2023-05-23 01:31:03 -0300

    aula sobre objetos

---
+++ JavaScript/Objetos/objetos.js
@@ -0,0 +1,14 @@
+let pessoa = {
+    nome: 'Leeh',
+    idade: 28,
+}
+
+console.log(pessoa)
+console.log(pessoa.idade)
+
+console.clear()
+
+// percorrer um objeto
+for ( let chave in pessoa) {
+    console.log(chave[nome])
+}
