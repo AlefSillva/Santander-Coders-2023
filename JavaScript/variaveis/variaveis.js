5099a31619fc47fc55c990a8cf531315846a5a0f
AlefSillva <alefsilva0295@gmail.com>
2023-05-23 01:35:13 -0300

    aula sobre variaveis

---
+++ JavaScript/variaveis/variaveis.js
@@ -0,0 +1,22 @@
+// VARIÁVEIS: São pedaços de memória para guardar informações.
+
+let idade = 27
+console.log(idade)
+
+// tipo de variáveis 
+idade = 28              // number
+let numero = 30          // number 
+let altura = 1.70;       // number
+let nome = 'Leeh'        // string
+let estudando = true     // tipo booleano
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+
+let outra;
+console.log(outra)
+
+
